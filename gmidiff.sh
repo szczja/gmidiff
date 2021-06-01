f5033381b6a80c7e3dc08530d9a906599e95c9b0
Szczeżuja <szja@protonmail.com>
2021-06-01 22:44:46 +0200

    Grep for headings and links

--- gmidiff.sh
+++ gmidiff.sh
@@ -11,7 +11,7 @@ function help() {
 # Function inspired on https://gitlab.com/uoou/dotfiles/-/blob/master/stow/bin/home/drew/.local/bin/lace
 function get() {
 	content=$(timeout 5 openssl s_client -crlf -quiet -connect "$1:1965" <<< "gemini://$2/" 2>/dev/null)
-	content=$(echo "$content" | grep "#")
+	content=$(echo "$content" | grep -E "(#)|(=>)")
 	hash=$(echo -n "$content" | sha256sum)
 	echo "$content"
 	echo "$hash" 		
