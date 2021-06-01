e39c5ba86589fc351c37abf9ca6dae6149535853
Szczeżuja <szja@protonmail.com>
2021-06-01 22:32:32 +0200

    Fix ommiting new lines, and add grep for headings only

--- gmidiff.sh
+++ gmidiff.sh
@@ -11,7 +11,10 @@ function help() {
 # Function inspired on https://gitlab.com/uoou/dotfiles/-/blob/master/stow/bin/home/drew/.local/bin/lace
 function get() {
 	content=$(timeout 5 openssl s_client -crlf -quiet -connect "$1:1965" <<< "gemini://$2/" 2>/dev/null)
-	echo "$content" 
+	content=$(echo "$content" | grep "#")
+	hash=$(echo -n "$content" | sha256sum)
+	echo "$content"
+	echo "$hash" 		
 	exit
 }
 
